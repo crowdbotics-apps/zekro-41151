960a2bee22938d8d9a9cfce38fdeff1c5aff67f5
Crowdbotics <team@crowdbotics.com>
2023-06-01 09:42:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "zekro-41151",
+  url: "https://zekro-41151.botics.co",
+  api: "https://zekro-41151.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
